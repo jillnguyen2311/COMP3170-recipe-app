ddcde0936fd51aa7fd96764db3e83c361ff45ba4
jillnguyen2311 <nguyennuduyhoai75@gmail.com>
2023-12-11 23:37:16 -0800

    apis added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import { useEffect, useState } from "react";
 import "./styles.css";
 import RecipeForm from "./Components/RecipeForm";
 import RecipeList from "./Components/RecipeList";
-import AddRecipeForm from "./Components/AddRecipeForm"; // Import the new component
+import AddRecipeForm from "./Components/AddRecipeForm";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Heading, Button } from "@chakra-ui/react";
 import { InventoryContext } from "./data/inventoryContext";
 
-const API_KEY = "f40e22bbc0c64e35b2da72f92aa38837";
+const API_KEYS = [
+  "447f5e20a3d54ff5be4d42d94cc85804",
+  "f40e22bbc0c64e35b2da72f92aa38837",
+  "c1007fe476f342669e8a3ba32c881031",
+  "b103f268819745d0b673abb444514395",
+  "730c494fdc83459ca37442b1a13cb36a",
+  "22d03274ed3d4424a154731f11db899a"
+];
 
 export default function App() {
+  const [apiKeyIndex, setApiKeyIndex] = useState(0);
   const [recipes, setRecipes] = useState(() => {
     const storedRecipes = localStorage.getItem("recipes");
     return storedRecipes ? JSON.parse(storedRecipes) : [];
@@ -19,23 +27,28 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [editing, setEditing] = useState(null);
+  const [recipeIdCounter, setRecipeIdCounter] = useState(0); // Counter for recipe IDs
 
   useEffect(() => {
     async function fetchRecipeData() {
       try {
         setLoading(true);
         const response = await fetch(
-          `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=8&page=${page}`
+          `https://api.spoonacular.com/recipes/random?apiKey=${API_KEYS[apiKeyIndex]}&number=8&page=${page}`
         );
         if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+          const errorStatus = response.status;
+          if (errorStatus === 402) {
+            switchApiKey();
+          }
         }
         const data = await response.json();
-        const newRecipes = data.recipes.map((recipe) => ({
+        const newRecipes = data.recipes.map((recipe, index) => ({
           ...recipe,
-          id: recipe.id || String(Math.random()),
+          id: recipeIdCounter + index, // Use the counter for sequential IDs
         }));
         setRecipes((prevRecipes) => [...prevRecipes, ...newRecipes]);
+        setRecipeIdCounter((prevCounter) => prevCounter + data.recipes.length); // Update the counter
         setPage((prevPage) => prevPage + 1);
         setLoading(false);
       } catch (error) {
@@ -47,11 +60,12 @@ export default function App() {
     if (recipes.length === 0 || page > 1) {
       fetchRecipeData();
     }
-  }, [page, recipes]);
+  }, [page, recipes, apiKeyIndex, recipeIdCounter]);
 
   function addRecipe(recipe) {
-    const newRecipes = [...recipes, recipe];
+    const newRecipes = [...recipes, { ...recipe, id: recipeIdCounter }];
     setRecipes(newRecipes);
+    setRecipeIdCounter((prevCounter) => prevCounter + 1); // Increment the counter
     localStorage.setItem("recipes", JSON.stringify(newRecipes));
     setEditing(null);
   }
@@ -73,6 +87,10 @@ export default function App() {
 
   function loadMoreRecipes() {
     setPage((prevPage) => prevPage + 1);
+  }
+
+  function switchApiKey() {
+    setApiKeyIndex((prevIndex) => (prevIndex + 1) % API_KEYS.length);
   }
 
   return (
